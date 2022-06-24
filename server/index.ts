ccf891c561be390a956580c96af3baa729646e1c
adrian <aapodaca@gmail.com>
2022-06-24 15:54:54 -0700

    initial commit

--- server/index.ts
+++ server/index.ts
@@ -1,22 +1,55 @@
-import { createServer } from 'http'
-import { parse } from 'url'
-import next from 'next'
+// import { createServer } from 'http'
+// import { parse } from 'url'
+// import next from 'next'
 
-const port = parseInt(process.env.PORT || '3000', 10)
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+// const port = parseInt(process.env.PORT || '3000', 10)
+// const dev = process.env.NODE_ENV !== 'production'
+// const app = next({ dev })
+// const handle = app.getRequestHandler()
 
-app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url!, true)
-    handle(req, res, parsedUrl)
-  }).listen(port)
+// app.prepare().then(() => {
+//   createServer((req, res) => {
+//     const parsedUrl = parse(req.url!, true)
+//     handle(req, res, parsedUrl)
+//   }).listen(port)
 
-  // tslint:disable-next-line:no-console
-  console.log(
-    `> Server listening at http://localhost:${port} as ${
-      dev ? 'development' : process.env.NODE_ENV
-    }`
-  )
-})
+//   // tslint:disable-next-line:no-console
+//   console.log(
+//     `> Server listening at http://localhost:${port} as ${
+//       dev ? 'development' : process.env.NODE_ENV
+//     }`
+//   )
+// })
+
+
+
+import express, { Request, Response } from "express";
+import next from "next";
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const port = process.env.PORT || 3000;
+
+(async () => {
+  try {
+    await app.prepare();
+    const server = express();
+    server.get('/api/test', (_req: Request, _res: Response) => {
+
+      _res.send('Express + TypeScript Server')
+      
+    })
+
+    server.all("*", (req: Request, res: Response) => {
+      return handle(req, res);
+    });
+    server.listen(port, (err?: any) => {
+      if (err) throw err;
+      console.log(`> Ready on localhost:${port} - env ${process.env.NODE_ENV}`);
+    });
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+})();
